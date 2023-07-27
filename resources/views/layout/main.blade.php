c8ae84f261d50e164e5c9a07789af1bdc4445a5a
wikomoko <mokoindonesia45@gmail.com>
2023-07-27 22:23:41 +0700

    setting template yield'

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,18 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Inferno</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Jost:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
 </head>
 <body>
     @include('layout.header')
-    <h1>ini body dari main templar</h1>
+    @yield('content')
     @include('layout.footer')
 
-    <script src="assets/js/script.js"></script>
-    <script src="assets/js/bootstrap.js"></script>
+    <script src="{{ asset('assets/js/script.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.js') }}"></script>
 </body>
 </html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.main')
+
+@section('content')
+    <h1 class="text-danger">ini udah dari pages</h1>
+@endsection
