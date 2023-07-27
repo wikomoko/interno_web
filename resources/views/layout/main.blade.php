c8d0e10302345421bdbdffaf697e344e1d65d00a
wikomoko <mokoindonesia45@gmail.com>
2023-07-27 21:55:21 +0700

    buat teplpate

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inferno</title>
+</head>
+<body>
+    @include('layout.header')
+    <h1>ini body dari main templar</h1>
+    @include('layout.footer')
+</body>
+</html>
