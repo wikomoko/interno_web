09a86d3f118520000502dc77579a0011316c044b
wikomoko <mokoindonesia45@gmail.com>
2023-07-31 09:55:49 +0700

    fixing mobile version on 'About Page'

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -15,7 +15,7 @@
             <div class="row">
                 <div class="col-md-2"></div>
                 <div class="col-md-8">
-                    <div class="quote_pad text-center">
+                    <div class="quote_pad text-center d-md-block d-none">
                         <span class="text-font-dm color-primary-2 text-size-85">“</span>
                         <p class="text-font-dm color-primary-2 text-size-35 text-italic">
                             I like an interior that defies labeling. I don't really want someone to walk into a room and
@@ -25,14 +25,30 @@
                             -BUNNY WILLIAMS
                         </p>
                     </div>
+                    <div class="quote_pad text-center d-md-none">
+                        <span class="text-font-dm color-primary-2 text-size-20">“</span>
+                        <p class="text-font-dm color-primary-2 text-size-20 text-italic">
+                            I like an interior that defies labeling. I don't really want someone to walk into a room and
+                            know that I did it
+                        </p>
+                        <p class="text-font-jost color-primary-4 text-size-16">
+                            -BUNNY WILLIAMS
+                        </p>
+                    </div>
                 </div>
                 <div class="col-md-2"></div>
             </div>
         </div>
     </section>
     <section>
-        <div class="container ">
-            <div class="row">
+        <div class="container">
+            <div class="row text-center text-md-start">
+                <div class="col-md-6 d-md-none">
+                    <div class="container">
+                        <img src="{{ asset('assets/img/page_about/wedo.jpg') }}" class="img-fluid img_round_rectange_pad"
+                            alt="">
+                    </div>
+                </div>
                 <div class="col-md-5 d-flex justify-content-between flex-column">
                     <p class="text-font-dm color-primary-2 text-size-50">What We Do</p>
                     <p class="text-font-jost color-primary-4 text-size-22">
@@ -48,7 +64,7 @@
                     </div>
                 </div>
                 <div class="col-md-1"></div>
-                <div class="col-md-6">
+                <div class="col-md-6 d-none d-md-block">
                     <div class="container">
                         <img src="{{ asset('assets/img/page_about/wedo.jpg') }}" class="img-fluid img_round_rectange_pad"
                             alt="">
@@ -57,7 +73,7 @@
             </div>
         </div>
         <div class="container margin_top_150">
-            <div class="row">
+            <div class="row text-center text-md-start">
                 <div class="col-md-6">
                     <div class="container">
                         <img src="{{ asset('assets/img/page_about/result.jpg') }}" class="img-fluid img_round_rectange_pad"
@@ -83,7 +99,8 @@
     </section>
     <section class="bg-color-primary-3">
         <div class="container text-center padding_vertical_180 margin_top_150">
-            <p class="text-font-dm text-size-50 color-primary-2">What the People Thinks <br />About Us</p>
+            <p class="text-font-dm text-size-50 color-primary-2 d-md-block d-none">What the People Thinks <br />About Us</p>
+            <p class="text-font-dm text-size-35 color-primary-2 d-md-none">What the People Thinks About Us</p>
             <div class="row">
                 <div class="col-md-3 mt-3">
                     <img src="{{ asset('assets/img/page_about/person_1.png') }}" class="img-fluid" alt="">
@@ -102,12 +119,13 @@
     </section>
     <section>
         <div class="container padding_vertical_180">
-            <p class="text-font-dm text-size-50 color-primary-2 text-center">Creative project? Let's have
+            <p class="text-font-dm text-size-50 color-primary-2 text-center d-none d-md-block">Creative project? Let's have
                 <br> a productive talk.
             </p>
+            <p class="text-font-dm text-size-35 color-primary-2 text-center d-md-none">Creative project? Let's have a productive talk.</p>
             <div class="row">
                 <div class="col-md-2"></div>
-                <div class="col-md-8">
+                <div class="col-md-8 mt-5">
                     <form action="#" method="POST" class="text-center">
                         @csrf
                         <div class="row">
